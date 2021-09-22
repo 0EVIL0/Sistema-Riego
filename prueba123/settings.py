6a8dc07b173bada270526256942b11b7c6d06e98
yesid steven vargas capera <yesidsvc@gmail.com>
2021-09-22 18:07:32 -0500

    actualizacion

--- prueba123/settings.py
+++ prueba123/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'prueba123.wsgi.application'
 DATABASES = {
             'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'prueba',
+            'NAME': 'q825pr4j0jgme2bm',
             'USER': 'xh5u7i10h847hd3a',
             'PASSWORD': 'dwdczjngyst69acc',
             'HOST': 'nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', #localhost 127.0.0.0 
