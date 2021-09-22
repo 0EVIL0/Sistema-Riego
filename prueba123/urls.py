9f5d3dbc6834eba7fad7de5790720875d6c07cc2
yesid steven vargas capera <yesidsvc@gmail.com>
2021-09-22 14:34:22 -0500

    agregue archivos para heroku

--- prueba123/urls.py
+++ prueba123/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-
 from django.urls import path, include, re_path
+
+from django.config import settings
+from django.config.urls.static import static
 
 urlpatterns = [
 path('admin/', admin.site.urls),
+
+#patc(r'courses/', include('course.urls')),
 # path('',views.index,name='Home'),
 
 path('',include('app.urls')),
-
-
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
